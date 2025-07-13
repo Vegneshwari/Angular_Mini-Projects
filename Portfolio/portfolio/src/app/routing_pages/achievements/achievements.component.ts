cbc9c867aee99a0fbc93d53f198171c70e3e6319
Vegneshwari <vickyvanitha24@gmail.com>
2025-07-13 21:30:03 +0530

    Portfolio website has been updated and fixed bugs. Working on the tsParticle

--- Portfolio/portfolio/src/app/routing_pages/achievements/achievements.component.ts
+++ Portfolio/portfolio/src/app/routing_pages/achievements/achievements.component.ts
@@ -1,13 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-achievements',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './achievements.component.html',
   styleUrl: './achievements.component.css'
 })
 export class AchievementsComponent {
 
+  images = [
+
+    {
+      src: 'assets/AE-2017.jpg',
+      caption: 'I was placed "second in Academic Excellence” in my third of college'
+    },
+    {
+      src:'assets/AE-2018.jpg',
+     caption: 'I was placed “first in Academic Excellence” in my final year of college'
+    },
+    {
+      src:'assets/Seminar.jpg',
+      caption: 'Actively participated in Seminars, Debates and other educational events, and placed first prise'
+    },
+    {
+      src: 'assets/Sport-DiscusThrow.jpg',
+      caption: 'Actively participated in Sports event and placed first prise'
+    }
+  
+  ]
+
+  currentSlideIndex = 0;
+  direction='';
+
+  prevSlide(): void{
+
+    this.direction="right";
+    this.currentSlideIndex= (this.currentSlideIndex-1+this.images.length)%this.images.length 
+
+  }
+
+  nextSlide():void{
+
+    this.direction="left";
+    this.currentSlideIndex = (this.currentSlideIndex+1+this.images.length)%this.images.length
+
+  }
+
+  setDirection(){
+    this.direction='';
+  }
+
+  // goToSlide(i:number){
+  //   this.currentSlideIndex = i;
+  // }
 }
--- Portfolio/portfolio/src/app/routing_pages/home/home.component.ts
+++ Portfolio/portfolio/src/app/routing_pages/home/home.component.ts
@@ -1,14 +1,59 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxParticlesModule } from '@tsparticles/angular';
+import { loadFull } from 'tsparticles';
+import { Engine } from '@tsparticles/engine';
+// import { Engine } from 'tsparticles-engine' --doubt;
+
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxParticlesModule],
   schemas: [NO_ERRORS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  particlesOptions = {
+    background: {
+      color: { value: '#1f1f1f' }
+    },
+    particles: {
+      number: { value: 50 },
+      color: { value: '#ffffff' },
+      links: {
+        enable: true, //draw lines between nearby particles
+        color: '#ffffff',
+        distance: 150
+      },
+      move: {
+        enable: true,
+        speed: 2
+      },
+      size: { value: 3 },
+      opacity: { value: 0.5 }
+    },
+    fullScreen: {
+      enable: true,
+      zIndex: -1
+    }
+  };
+
+
+  async particlesInit(engine: Engine): Promise<void>
+    {
+      await loadFull(engine)
+    }
+
+  particlesLoaded(container: any): void
+  {
+    console.log('Particles loaded:', container);
+    
+
+  }
 
 }
